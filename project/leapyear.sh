2561d2f0938e1214c6c6024162981629b3ce30e8
AbhishekKadam1980 <abhi.kadam62@gmail.com>
2021-11-26 00:32:08 +0530

    Added modifications to leap year file

--- project/leapyear.sh
+++ project/leapyear.sh
@@ -1,13 +1,31 @@
 #!/bin/bash 
-
 i=0
 while (( i<5 ));do
 	echo "Please enter year:"
 	read year
+	 
+	case ${#year} in
+		5)
+			echo "Please enter a valid year"
+			exit 0
+			;;
+		3)
+			echo "Too short"
+			exit 0
+			;;
+   
+		*)
+			echo "Error"
+			exit 0
+			;;
+	esac
 	
+	
+
 	(($year%4 == 0)) && (($year%100 != 0))|| (($year%400 == 0)) &&  echo "Leap year"|| echo "Not leap year"
 	((i++))
 	
+
 	echo "$i iteration completed" 
 	echo "Would you like to continue?"
 	read  user_input	
@@ -18,6 +36,7 @@ while (( i<5 ));do
 			continue
 			;;
 		'n')
+			echo "Program Terminated. BYE"
 			break
 			;;
 		*)
