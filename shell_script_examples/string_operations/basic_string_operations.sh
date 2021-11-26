62bfd787f1d5a3edc8bbb748bee9bebe3aa38b2b
AbhishekKadam1980 <abhi.kadam62@gmail.com>
2021-11-26 12:36:21 +0530

    Made some comments to basic_string_operation.sh file

--- shell_script_examples/string_operations/basic_string_operations.sh
+++ shell_script_examples/string_operations/basic_string_operations.sh
@@ -2,30 +2,31 @@
 string=Abhishek
 string2=chapelle
 
-#how to check the length of the string#
-echo ${#string}
-echo 
 
-#get the last 4 alphabets of the string#
+#	NOTE :- NONE OF THIS IS POSSIBLE WITHOUT { } VERY COMPULSORY
+#how to check the length of the string
+#usage of { } is compulsory, () or [] do not work 
+
+echo ${#string} 
+
+#delete the first 4 alphabets of the string
 echo ${string:4}
-echo
 
-#start at 4 end at 6#
+#delete 4 and show the next 2 characters
 echo ${string:4:2}
-echo
 
-#delete from the beginning and stop at the first e,#
+#Traverse string from left to right and delete till first encountered 'e' then show the remaining string in any available
+#Notice the usage of * when going from left to right, It is invoked before specifying the character.
 echo ${string2#*e}
-echo
 
-#delete from the left and go to the last e#
+#Traverse the string from left to right till the last instance of 'e' and delete the traversed path, Show remaining path if any available.
+
 echo ${string2##*e}
-echo
 
-#delete from the right and stop at the first e#
+#Traverse from the right to left and stop at the first e,Display remaining string if any available.
+#Notice usage of * when going from right to left.It is invoked in the end.
 echo ${string2%e*}
-echo
 
-#delete from the right till the last e#
+#Traverse from the right to left till the last instance of 'e' and delete, Display remaining string if any.
 echo ${string2%%e*}
-echo
+
